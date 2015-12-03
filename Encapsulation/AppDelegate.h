3e4e2742f2d0dfaacedf82c4aa5a37922d8ae9c5
Phuong <phuongnhu0894@gmail.com>
2015-12-03 09:56:40 +0700

    First Commit

---
+++ Encapsulation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Encapsulation
+//
+//  Created by student on 12/3/15.
+//  Copyright © 2015 Techmaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Encapsulation/ViewController.m
@@ -0,0 +1,43 @@
+//
+//  ViewController.m
+//  Encapsulation
+//
+//  Created by student on 12/3/15.
+//  Copyright © 2015 Techmaster. All rights reserved.
+//
+
+#import "ViewController.h"
+//#import "OptionView.h"
+
+@interface ViewController () <UITableViewDataSource, UITableViewDelegate>
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+//@property (strong, nonatomic) UIView *optionView;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 10;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellIdentifier = @"customCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
+    }
+    
+    cell.textLabel.text = [NSString stringWithFormat:@"Row %li", indexPath.row];
+    return cell;
+}
+
+@end
